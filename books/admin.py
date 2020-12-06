e293f9cd1764bdf631e836dc7db0dfc01b686124
Xiaoxia <michaelyaonz@gmail.com>
2020-12-06 10:38:46 +0800

    allquizzes

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Books
+from .models import Question, Quizzes, Answer, Category
 
-admin.site.register(Books)
+admin.site.register(Question)
+admin.site.register(Quizzes)
+admin.site.register(Answer)
+admin.site.register(Category)
--- books/schema.py
+++ books/schema.py
@@ -1,20 +1,38 @@
 import graphene
-from graphene_django import DjangoObjectType
-from .models import Books
+from graphene_django import DjangoObjectType, DjangoListField
+from .models import Quizzes, Answer, Category, Question
 
 
-class BooksType(DjangoObjectType):
+class CategoryType(DjangoObjectType):
     class Meta:
-        model = Books
-        fields = ("id", "title", "excerpt")
+        model = Category
+        fields = ("id", "name")
+
+
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ("id", "title", "category", "question")
+
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ("id", "title", "quiz", "answer")
+
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ("id", "question", "answer_text")
 
 
 class Query(graphene.ObjectType):
 
-    all_books = graphene.List(BooksType)
+    all_quizzes = DjangoListField(QuizzesType)
 
-    def resolve_all_books(root, info):
-        return Books.objects.all()
+    # def resolve_all_quizzes(root, info):
+    #     return
 
 
 schema = graphene.Schema(query=Query)
