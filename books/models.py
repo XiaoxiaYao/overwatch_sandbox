4a9460093e89bdb1aa60f80ccb8ee5c87b7dbe87
Xiaoxia <michaelyaonz@gmail.com>
2020-12-11 10:04:29 +0800

    sdfdsf

--- books/models.py
+++ books/models.py
@@ -49,7 +49,7 @@ class Question(models.Model):
         default=False, verbose_name=_("Active Status")
     )
 
-# commentsdf
+# commentsdfsdfd
 class Answer(models.Model):
 
     question = models.ForeignKey(
