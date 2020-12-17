5b1d5eae68060c3ab452d16dd893271f489d1462
Xiaoxia <michaelyaonz@gmail.com>
2020-12-17 10:22:53 +0800

    Update models.py

--- books/models.py
+++ books/models.py
@@ -49,7 +49,7 @@ class Question(models.Model):
         default=False, verbose_name=_("Active Status")
     )
 
-# commentsdfsdfdsdfsdf
+# commentsdfsdfdsdfsdf Cool
 class Answer(models.Model):
 
     question = models.ForeignKey(
